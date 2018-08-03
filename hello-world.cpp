93c24140455060163c118fddd07b899e55466203
Vasiliy <golovachko@gmail.com>
2018-08-03 21:23:55 +0300

    hotfix commit

--- hello-world.cpp
+++ hello-world.cpp
@@ -4,12 +4,13 @@
 #include "stdafx.h"
 
 // new freihaupt comment
-// local master comment
+// this is a hotfix
 
 int main()
 {
 	std::wcout << L"test-dev-branch" << std::endl;
-	std::wcout << L"another line" << std::endl;
+
+	std::wcout << L"hotfix!" << std::endl;
 	return 0;
 }
 
